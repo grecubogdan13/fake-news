b7c2dc97d7b43f457f1a9656cfe5015e57054e3f
grecubogdan13 <74433377+grecubogdan13@users.noreply.github.com>
2024-04-26 10:44:23 +0100

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -2,14 +2,12 @@ import pandas as pd
 import nltk
 import re
 
-# Read the CSV file
 df = pd.read_csv("./input/input_tweets.csv")
 df = df.sort_values(by="tweet", key=lambda x: x.str.len())
 
-nltk.download('punkt')  # Download the necessary resources if not already downloaded
+nltk.download('punkt')  
 
 def split_into_sentences(text):
-    # Tokenize the text into sentences
     sentences = nltk.sent_tokenize(text)
     return sentences
 
@@ -21,7 +19,6 @@ for index, row in df.iterrows():
     label = row["label"]
     
     if "vacc" in tweet_text.lower():
-        # print(tweet_text)
         tweet_text = re.sub(r"@([A-Za-z0-9_]{1,15})",'', tweet_text)
         tweet_text = re.sub(r"#([A-Za-z0-9_]+)",'', tweet_text)
         tweet_text = re.sub(r"(?:https?:\/\/)?(?:www\.)?[A-Za-z0-9\-]+\.[A-Za-z]{2,}(?:\/\S*)?",'', tweet_text)
@@ -31,9 +28,6 @@ for index, row in df.iterrows():
         tweet_text = re.sub(r'\.','. ', tweet_text)
         tweet_text = re.sub(r'\"[A-Za-z0-9_]+\"','', tweet_text)
 
-        # print(tweet_text)
-        # doc = nlp(tweet_text)
-        # for sent in doc.sents:
         sentences = split_into_sentences(tweet_text)
         for sent in sentences:
             if "vacc" in sent.lower() and not sent.strip().endswith("?"):
